eeaaebe76ffd571fb7110f106505f25fd0e61526
Rebwon <msolo021015@naver.com>
2019-11-23 20:30:36 +0900

    gs-springboot-oauth2-study(logout)

--- src/main/java/ko/maeng/gsoauth2demo/GsOauth2DemoApplication.java
+++ src/main/java/ko/maeng/gsoauth2demo/GsOauth2DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,12 +30,10 @@ public class GsOauth2DemoApplication extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.antMatcher("/**")
-			.authorizeRequests()
-				.antMatchers("/", "/login**", "/webjars/**", "/error**")
-				.permitAll()
-			.anyRequest()
-				.authenticated();
+		// @formatter:off
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll().anyRequest()
+						.authenticated().and().logout().logoutSuccessUrl("/").permitAll().and().csrf()
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		// @formatter:on
 	}
 }
